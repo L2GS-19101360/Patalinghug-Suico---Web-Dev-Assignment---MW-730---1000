3f785cab59f5f918ac1b457cc8f3c7adf755232c
Lorenz Gil Suico <125742061+L2GS-19101360@users.noreply.github.com>
2023-04-19 09:24:09 +0800

    Update check_connection.php

--- check_connection.php
+++ check_connection.php
@@ -1,5 +1,17 @@
 <?php
 
+$serverName = "localhost";
+$databaseName = "patalinghug-suico_web dev assignment_(mw 730-1000)_db";
+$userName = "root";
+$password = "";
 
+$est_conn = new mysqli($serverName, $userName, $password, $databaseName);
+
+if ($est_conn->connect_error){
+    echo "No";
+}
+
+//Check if the connection to the database is established
+echo "";
 
 ?>
